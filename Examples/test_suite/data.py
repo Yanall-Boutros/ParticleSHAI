a2ab626a8ba065924ab6c4dfefa15f7524408878
Yanall-Boutros <yboutros@ucsc.edu>
2020-07-13 22:49:26 -0700

    Updated debug code

--- Examples/test_suite/data.py
+++ Examples/test_suite/data.py
@@ -12,13 +12,17 @@ from pyjet import cluster
 # Initalize
 # -----------------------------------------------------------------------
 pid                   = 'pdgid'
-bjet_id               = 5
 num_events            = 1000 # Number of events to process per parent
-test                  = 100  # particle. Number of test events to reserve
-discarded_data        = []   # Archive of any particles discarded
 # -----------------------------------------------------------------------
 # Function Definitions
 # -----------------------------------------------------------------------
+class NoneTypes_in_event():
+    def __init__(self, nn, nt, pcnts, hpids):
+        self.num_none = nn
+        self.num_total = nt
+        self.percents = pcnts
+        self.hist_pids = hpids
+
 def is_massless_or_isolated(jet, i):
     # Returns true if a jet is only constituated of one particle
     # (nconsts == 1) and has a pdgid equal to that
@@ -26,41 +30,52 @@ def is_massless_or_isolated(jet, i):
     noneflag = False
     if jet.userinfo is None:
         noneflag = True
-        print("Nonetype in userinfo, jet i = ", i, "Inside inner function call") 
-    else: print("Sometype in userinfo, jet i = ", i, "Inside inner function call")
     if len(jet.constituents_array()) == 1: 
         # Count number of bjets
         if noneflag and jet.userinfo is not None:
             print("what, why?")
-        else: print("Sometype in userinfo, jet i = ", i, "jet.userinfo[pid] = ",
-                jet.userinfo[pid])
     return False
 
-def pythia_sim(cmd_file, part_name=""):
-    # The main simulation. Takes a cmd_file as input. part_name 
-    # is the name of the particle we're simulating decays from.
-    # Only necessary for titling graphs.
-    # Returns an array of 2D histograms, mapping eta, phi, with transverse
-    # energy.
-    pythia = Pythia(cmd_file, random_state=1)
-    selection = ((STATUS == 1) & ~HAS_END_VERTEX)
-    unclustered_particles = []
-    num_b_jets_per_event  = []
+def NoneType_statistics(jets, debug_data):
+    num_nonetypes = 0
+    hist_pids = {}
+    for jet in jets:
+        if jet.userinfo is None: num_nonetypes += 1
+        else:
+            label = np.abs(jet.userinfo[pid])
+            if hist_pids.get(label) is None:
+                hist_pids[label] = 1
+            else:
+                hist_pids[label] += 1
+    data = NoneTypes_in_event(num_nonetypes, len(jets),
+        num_nonetypes/len(jets), hist_pids)
+    debug_data.append(data)
+    return ("Number of jet.userinfo NoneTypes: " + str(num_nonetypes) +
+            "\nTotal Number of Jets: " + str(len(jets)) +
+            "\nPercent NoneType: " + str(num_nonetypes/len(jets)) +
+            "\nPID count: " + hist_pids.__str__() + "\n" +
+            "-"*72)
+
+def pythia_sim(cmd_file):
+    # The main simulation. Takes a cmd_file as input. 
+    pythia                   = Pythia(cmd_file, random_state=1)
+    selection                = ((STATUS == 1) & ~HAS_END_VERTEX)
+    unclustered_particles    = []
     for event in pythia(events=num_events):
-        vectors              = event.all(selection)
-        sequence             = cluster(vectors, R=0.4, p=-1, ep=True) 
-        jets                 = sequence.inclusive_jets()
+        vectors               = event.all(selection)
+        sequence              = cluster(vectors, R=0.4, p=-1, ep=True) 
+        jets                  = sequence.inclusive_jets()
+        print(NoneType_statistics(jets, debug_data))
+        # 
+        # Vestigial code that I didn't delete because at first I thought
+        # the context might be useful, I do not believe that to still be
+        # the case.
+        #
         unclustered_particles.append(sequence.unclustered_particles())
-        num_b_jets  = 0
-        user_info_none_count = 0
         for i, jet in enumerate(jets):
-            if jet.userinfo is None:
-                print("Nonetype in userinfo, jet i = ", i)
-            else: print("Sometype in userinfo, jet i = ", i, "Outside inner function call")
-            if is_massless_or_isolated(jet, i):
-                pass
+            if is_massless_or_isolated(jet, i): pass
 # -----------------------------------------------------------------------
 # Main process for generating tensor data
-# -----------------------------------------------------------------------
-higgs_ww_data    = pythia_sim('higgsww.cmnd', "higgsWW")
-higgs_zz_data    = pythia_sim('higgszz.cmnd', 'higgsZZ')
+debug_data = []
+pythia_sim('ff2hfftww.cmnd')
+
